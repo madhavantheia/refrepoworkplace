df98b80dd636f33bee26e7e0a7f1aba18193bd93
Madhavan <madhavan.profession@gmail.com>
2020-06-21 18:45:54 +0530

    post update done

--- src/test/java/org/facebook/products/pages/Home.java
+++ src/test/java/org/facebook/products/pages/Home.java
@@ -10,7 +10,7 @@ import org.testng.Assert;
 
 public class Home extends PageObject {
 
-    @FindBy(xpath = "//*[@id=\"u_0_1\"]/div/div/div/div/div/div[2]/div[2]/div[2]/header/div/div/div[1]/div[2]/div/h1/span")
+    @FindBy(xpath = "//span[.='News Feed']")
     WebElement newsFeedLabel;
 
     @SuppressWarnings("SpellCheckingInspection")
@@ -20,7 +20,7 @@ public class Home extends PageObject {
     @FindBy(xpath = "//span[contains(@class, ' _50f9  _50f7 _2iem')][.='General']")
     WebElement generalGroup;
 
-    @FindBy(xpath = "//input[contains(@class, '_2cuy _3dgx']")
+    @FindBy(xpath = "//div[(@class='_2cuy _3dgx')]")
     WebElement messageEditor;
 
     @FindBy(xpath = "//span[.='Post']")
@@ -36,25 +36,36 @@ public class Home extends PageObject {
         try {
             Assert.assertTrue(labelname.contains("News Feed"), "\"News Feed\" not available");
         } catch (AssertionError ae) {
+
             Hooks.verificationErrors.append("\n\n").append(ae.toString());
         }
         System.out.println("\nUsing POM; pages.Landing.validateNewsFeed\n");
     }
 
+    /***
+     * JavascriptExecutor js = (JavascriptExecutor) driver;
+     * js.executeScript("window.scrollBy(0,1000)");
+     * removed as scrolling to the bottom of page was
+     * interrupting the createPostEditor field's click operation
+     *
+     * examine below throws exception handling in documentation comments
+     *
+     * @throws InterruptedException
+     */
+
     public void postStatus() throws InterruptedException {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollBy(0,1000)");
         createPostEditor.click();
         System.out.println("Editor accessed");
         Hooks.wait.until(ExpectedConditions.visibilityOf(generalGroup));
         generalGroup.click();
         System.out.println("General group selected");
-        Thread.sleep(5000);
-
+        Hooks.wait.until(ExpectedConditions.visibilityOf(messageEditor));
         messageEditor.click();
         System.out.println("message editor clicked");
-        messageEditor.sendKeys("Posted using robotizing script");
-        Thread.sleep(5000);
+        messageEditor.sendKeys("Posted using robotizing script 18-June");
+        System.out.println("Message typed");
         postButton.click();
+        Thread.sleep(5000);
+        System.out.println("posted the message");
     }
 }
--- src/test/java/org/facebook/products/runner/TestRunner.java
+++ src/test/java/org/facebook/products/runner/TestRunner.java
@@ -3,10 +3,11 @@ package org.facebook.products.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-/* Entire code from AbstractTestNGCucumberTests has to be implemented in here
-though we chose a nack approach of extending it
-For better understanding go through AbstractTestNGCucumberTests.class, TestNGCucumberRunner.java & CucumberOptions.java
-all are available in io.cucumber.testng
+/**
+ * Entire code from AbstractTestNGCucumberTests has to be implemented in here
+ * though we chose a nack approach of extending it
+ * For better understanding go through AbstractTestNGCucumberTests.class, TestNGCucumberRunner.java & CucumberOptions.java
+ * all are available in io.cucumber.testng
  */
 
 @CucumberOptions(
